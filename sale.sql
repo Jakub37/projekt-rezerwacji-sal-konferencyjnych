ff2fbfebbd9863d48f9ab4ee1797959888a892eb
jakub <jakubjan252@gmail.com>
2025-09-30 11:10:22 +0200

    koniec

--- sale.sql
+++ sale.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Wrz 30, 2025 at 10:27 AM
+-- Generation Time: Wrz 30, 2025 at 11:07 AM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -38,6 +38,24 @@ CREATE TABLE `sale` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 --
+-- Dumping data for table `sale`
+--
+
+INSERT INTO `sale` (`id`, `nr_sali`, `data`, `od_godziny`, `rezerwacja`, `do_godziny`, `id_uzytkownika`) VALUES
+(24, 1, '2025-09-24', '13:00:00', 'jakub kowal', '15:00:00', 156),
+(26, 1, '2025-09-24', '10:00:00', 'jakub kowal', '13:00:00', 156),
+(28, 2, '2025-09-24', '17:00:00', 'jakub kowal', '18:54:00', 161),
+(29, 2, '2025-09-06', '12:32:00', 'jakub kowal', '15:13:00', 156),
+(30, 1, '2025-09-11', '03:12:00', 'jakub kowal', '03:02:00', 156),
+(35, 1, '2025-10-04', '08:00:00', 'jakub kowal', '09:00:00', 156),
+(36, 1, '2025-10-06', '12:00:00', 'jakub kowal', '14:00:00', 156),
+(37, 2, '2025-09-29', '13:00:00', 'jakub kowal', '14:00:00', 156),
+(38, 1, '2025-09-01', '10:00:00', 'jakub kowal', '16:00:00', 156),
+(40, 1, '2025-09-29', '14:00:00', 'jakub kowal', '15:00:00', 156),
+(42, 2, '2025-10-04', '12:32:00', 'jakub kowal', '13:23:00', 156),
+(43, 2, '0000-00-00', '13:13:00', 'jakub kowal', '13:42:00', 161);
+
+--
 -- Indeksy dla zrzutów tabel
 --
 
@@ -55,7 +73,7 @@ ALTER TABLE `sale`
 -- AUTO_INCREMENT for table `sale`
 --
 ALTER TABLE `sale`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
